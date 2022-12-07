35bde3b9b228d5e06d31c9db229506e4b402496d
Huy Duong Ngoc Quang <huy.duonghe1nz@hcmut.edu.vn>
2022-12-08 00:15:04 +0700

    Template Update

--- EcomApp/base/urls.py
+++ EcomApp/base/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomeView, LoginView, CartView, ProductView
+from .views import HomeView, LoginView, CartView, ProductView, RegisterView
 
 urlpatterns = [
     path('', HomeView, name='home'),
     path('login/', LoginView, name='login'),
+    path('register/', RegisterView, name='register'),
     path('cart/', CartView, name='cart'),
     path('product/', ProductView, name='product')
 ]
