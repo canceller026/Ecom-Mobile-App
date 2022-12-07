139058c4f8f20950a8daeaa353837caacfa6585a
Huy Duong Ngoc Quang <huy.duonghe1nz@hcmut.edu.vn>
2022-12-07 13:14:04 +0700

    Database script update

--- EcomApp/base/models.py
+++ EcomApp/base/models.py
@@ -99,6 +99,7 @@ class UserProfile(models.Model):
     name = models.CharField(unique=True, max_length=255)
     address = models.TextField(blank=True, null=True)
     role = models.CharField(max_length=1)
+    user = models.ForeignKey('AuthUser', models.DO_NOTHING, blank=True, null=True)
 
     class Meta:
         managed = False
